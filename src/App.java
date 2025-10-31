7da7d074380b42771c0c98f654f9ff3f1ba2bc25
Santiago Luque <luquevillanuevasantiago@gmail.com>
2025-10-31 12:29:54 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -3,15 +3,14 @@ public class App {
         int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la pirámide: "));
         String c = System.console().readLine("Introduzca el carácter de relleno: ");
 
-        for (int i = 0; i < altura; i++) {
-            for (int j = altura; j > i; j--) {
+        for (int fila = 0; fila < altura; fila++) {
+            for (int j = altura; j > fila; j--) {
                 System.out.print(" ");
             }
-            for (int j = 0; j <= 2 * i; j++) {
-                System.out.print("*");
+            for (int j = 0; j <= 2 * fila; j++) {
+                System.out.print(c);
             }
             System.out.println();
         }
-        
     }
 }
