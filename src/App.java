578322559a110eb0d876c9ec61b6adc546aed5c7
Santiago Luque <luquevillanuevasantiago@gmail.com>
2025-10-31 11:07:13 +0100

    A la mitad

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la pirámide: "));
+        String c = System.console().readLine("Introduzca el carácter de relleno: ");
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = altura; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= 2 * i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
     }
 }
